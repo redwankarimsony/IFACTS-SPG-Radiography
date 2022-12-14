299578d1313c0115a355671e14ea0da3866bebf1
redwankarimsony <redwankarimsony1455@gmail.com>
2022-12-13 23:09:04 -0500

    added multiple image view with labels

--- Matcher-GUI/demo.py
+++ Matcher-GUI/demo.py
@@ -1,25 +1,41 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget
+import os.path as osp
+import glob
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+import sys
 
-class Example(QWidget):
-
-    def __init__(self, imageUrl):
+class SingleID(QWidget):
+    def __init__(self, imageFolder, ID):
         super().__init__()
+        imageFiles = glob.glob(f"{imageFolder}/{ID}*.jpg")
+        layout = QHBoxLayout()
+        for imageFile in imageFiles:
+            layout.addWidget(self.getImageWidget(imageURL=imageFile))
+        self.setLayout(layout)
 
-        self.im = QPixmap(imageUrl)
-        self.label = QLabel()
-        self.label.setPixmap(self.im)
 
-        self.grid = QGridLayout()
-        self.grid.addWidget(self.label,1,1)
-        self.setLayout(self.grid)
+    @staticmethod
+    def getImageWidget(imageURL):
+        """
+        This method takes the URL of an image and then makes an 
+        QtWidget with image and image name at the bottom
+        """
+        layout = QVBoxLayout()
+        imageLabel = QLabel()
+        widget = QLabel()
+        imageLabel.setPixmap(QPixmap(imageURL).scaled(512, 512, Qt.KeepAspectRatio))
+        layout.addWidget(imageLabel)
+        textLabel = QLabel(imageURL.split("/")[-1])
+        textLabel.setAlignment(Qt.AlignCenter)
+        textLabel.setStyleSheet("border: 1px solid black;")
+        layout.addWidget(textLabel)
+        widget.setLayout(layout)
+        return widget
 
-        self.setGeometry(50,50,320,200)
-        self.setWindowTitle("PyQT show image")
-
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
+    window = SingleID(imageFolder="../Annotation/images_annotation_1", ID="012")
+    window.show()
     sys.exit(app.exec_())
