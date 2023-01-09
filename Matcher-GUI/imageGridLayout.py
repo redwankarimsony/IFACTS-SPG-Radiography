54ffecb0b29dc15c160e730e2b9c03b09479e705
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-01-09 13:58:07 -0500

    Added the folder selection method

--- Matcher-GUI/imageGridLayout.py
+++ Matcher-GUI/imageGridLayout.py
@@ -6,26 +6,43 @@ from PyQt5.QtGui import QPixmap, QFont, QColor
 from PyQt5.QtCore import Qt
 import sys
 from config import config
-
+from imageDisplayWidget import SingleID
 
 
 class ImageGrid(QWidget):
     def __init__(self, imagePaths):
         super().__init__()
+        self.imagePaths = imagePaths
         self.mainLayout = QGridLayout()
-        self.mainLayout.addWidget(QLabel("Redwan"), 0, 0)
-        self.mainLayout.addWidget(QLabel('green'), 1, 0)
-        self.mainLayout.addWidget(QLabel('blue'), 1, 1)
-        self.mainLayout.addWidget(QLabel('purple'), 2, 1)
         self.setLayout(self.mainLayout)
-        self.mainLayout.addWidget(QLabel("This is a nice layout"))
+
+        self.num_cols = 3
+
+        self.initGUI()
+
+    def initGUI(self):
+        idx = 0
+
+        if len(self.imagePaths):
+            for row in range((len(self.imagePaths)//self.num_cols)+1):
+                for column in range(self.num_cols):
+                    widget = SingleID.getImageWidget(self.imagePaths[idx])
+                    print(row, column)
+                    self.mainLayout.addWidget(widget, row, column)
+                    idx += 1
+                    if idx >= len(self.imagePaths):
+                        # break the inner loop in columns
+                        break
+                if idx >= len(self.imagePaths):
+                    # break the outer loop for row stop
+                    break
 
 
 if __name__ == '__main__':
+    image_paths = glob.glob("/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/images_annotation_1/*.jpg")
+    print(image_paths)
     app = QApplication(sys.argv)
-    window = ImageGrid()
+    window = ImageGrid(imagePaths=image_paths[:5])
+    window.setFixedSize(1920, 1080)
     window.show()
     sys.exit(app.exec_())
-
-
-
--- Matcher-GUI/verification-app.py
+++ Matcher-GUI/verification-app.py
@@ -2,13 +2,12 @@ import os
 import sys
 import os.path as osp
 import glob
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout, QDesktopWidget, QGridLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout, QDesktopWidget, \
+    QGridLayout, QFileDialog
 from PyQt5.QtGui import QPixmap, QFont, QIcon
 from PyQt5.QtCore import Qt
 from config import config
 from imageDisplayWidget import SingleID
-
-
 
 
 class SearchEngine(QWidget):
@@ -17,8 +16,7 @@ class SearchEngine(QWidget):
         self.imageFolder = imageFolder
         sizeObject = QDesktopWidget().screenGeometry(-1)
         self.setWindowIcon(QIcon(config.appIcon))
-        self.setFixedSize(int(sizeObject.width()*.7), int(sizeObject.height()*.7))
-
+        self.setFixedSize(int(sizeObject.width() * .7), int(sizeObject.height() * .7))
 
         self.mainLayout = QHBoxLayout()
         self.inputLayout = QVBoxLayout()
@@ -47,13 +45,13 @@ class SearchEngine(QWidget):
         self.PMXrays = self.getAll_PM_Xrays(self.imageFolder, phase="PM")
         self.AMXrays = self.getAll_PM_Xrays(self.imageFolder, phase="AM")
 
-        print(osp.join(self.imageFolder , self.PMXrays[0]))
-        widget = SingleID.getImageWidget(osp.join(self.imageFolder , self.PMXrays[0]))
+        print(osp.join(self.imageFolder, self.PMXrays[0]))
+        widget = SingleID.getImageWidget(osp.join(self.imageFolder, self.PMXrays[0]))
         widget.setFixedWidth(800)
         self.inputDispLayout.addWidget(widget)
 
 
-
+        self.nextBtn.clicked.connect(self.openFileNameDialog)
 
     @staticmethod
     def getAll_PM_Xrays(imgDir, phase="PM"):
@@ -70,27 +68,43 @@ class SearchEngine(QWidget):
             print("ERROR: Image Directory does not exists!")
             return []
 
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,
+                                                  "QFileDialog.getOpenFileName()",
+                                                  "",
+                                                  "All Files (*);;Python Files (*.py)",
+                                                  options=options)
+        if fileName:
+            print(fileName)
 
+    def openFileNamesDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        files, _ = QFileDialog.getOpenFileNames(self,
+                                                "QFileDialog.getOpenFileNames()",
+                                                "",
+                                                "All Files (*);;Python Files (*.py)",
+                                                options=options)
+        if files:
+            print(files)
 
-
-
-
-
-
-
-
-
-
+    def saveFileDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(self,
+                                                  "QFileDialog.getSaveFileName()",
+                                                  "",
+                                                  "All Files (*);;Text Files (*.txt)",
+                                                  options=options)
+        if fileName:
+            print(fileName)
 
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     window = SearchEngine(imageFolder=config.imageDir)
     # window.setGeometry(10, 10, 1920, 800)
     window.show()
     sys.exit(app.exec_())
-
-
-
-
