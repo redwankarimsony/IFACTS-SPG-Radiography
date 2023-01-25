89aa5c4f7df1ecae71395c84991309f0cc7a673c
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-01-24 20:49:48 -0500

    updated the script

--- CXR-Patient-ReID/PatientVerification/enhancement_experiments.py
+++ CXR-Patient-ReID/PatientVerification/enhancement_experiments.py
@@ -5,18 +5,30 @@ import matplotlib.pyplot as plt
 
 color = ['b', 'g', 'r']
 
+
 def getColorHist(image):
-    if len(image.shape) ==3:
-        hists= []
+    """
+    This function produces channelwise histogram distribution.
+    """
+    if len(image.shape) == 3:
+        hists = []
         for i, col in enumerate(color):
             hists.append(cv2.calcHist([image], [i], None, [256], [0, 256]))
-
+        return hists
+    else:
+        hists = []
+        i = 0
+        hists.append(cv2.calcHist([image], [i], None, [256], [0, 256]))
         return hists
 
 
-
-
-def applyCLAHE(image, display=False):
+def applyCLAHE(image, display: bool = False):
+    """
+    CLAHE implementation.
+    image: 3-channel grayscale image.
+    returns the image applied CLAHE.
+    display: if True, it will display the input and output.
+    """
     image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # The declaration of CLAHE
@@ -24,7 +36,6 @@ def applyCLAHE(image, display=False):
     clahe = cv2.createCLAHE(clipLimit=5)
     final_img = clahe.apply(image_bw)  # + 30
     final_img = np.stack((final_img,) * 3, axis=-1)
-
 
     # Ordinary thresholding the same image
     _, ordinary_img = cv2.threshold(image_bw, 155, 255, cv2.THRESH_BINARY)
@@ -45,7 +56,6 @@ def applyCLAHE(image, display=False):
         plt.axis('off')
         plt.title("Binary Thresholded Image")
 
-
         # Display the CLAHE processed image
         fig.add_subplot(rows, cols, 3)
         plt.imshow(final_img, cmap=plt.cm.gray)
@@ -56,14 +66,18 @@ def applyCLAHE(image, display=False):
     return final_img
 
 
-def applyHistogramEqualization(image, display=False):
+def applyHistogramEqualization(image, display: bool = False):
+    """
+    Applies the histogram equalization to a 3 channel grayscale image.
+    If display is true, it wills show the comparison.
+    """
     # https://123machinelearn.wordpress.com/2017/12/25/image-enhancement-using-high-frequency-emphasis-filtering-and-histogram-equalization/
     image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     print(image.shape)
 
     hist, bins = np.histogram(image_bw.flatten(), bins=256, range=[0, 256])
     cdf = hist.cumsum()
-    cdf_norm = cdf * hist.max()/cdf.max()
+    cdf_norm = cdf * hist.max() / cdf.max()
 
     cdf_m = np.ma.masked_equal(cdf, 0)
     cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
@@ -76,7 +90,9 @@ def applyHistogramEqualization(image, display=False):
 
         # Display the original image
         fig.add_subplot(rows, cols, 1)
-        plt.imshow(image, cmap=plt.cm.gray); plt.axis('off'); plt.title("Input Image")
+        plt.imshow(image, cmap=plt.cm.gray);
+        plt.axis('off');
+        plt.title("Input Image")
 
         hists = getColorHist(image)
         fig.add_subplot(rows, cols, 3)
@@ -85,7 +101,6 @@ def applyHistogramEqualization(image, display=False):
         plt.plot(hists[2], color="r")
         plt.xlim([0, 256])
         plt.title("Original Histogram")
-
 
         # Display the thresholded image
         fig.add_subplot(rows, cols, 2)
@@ -101,10 +116,7 @@ def applyHistogramEqualization(image, display=False):
         plt.xlim([0, 256])
         plt.title("Equalized Histogram")
 
-
-
         plt.show()
-
 
         plt.plot
 
@@ -112,9 +124,12 @@ def applyHistogramEqualization(image, display=False):
     return img_enhanced
 
 
-
-def applyHFEFilter(image, display=False):
-    #https://123machinelearn.wordpress.com/2017/12/25/image-enhancement-using-high-frequency-emphasis-filtering-and-histogram-equalization/
+def applyHFEFilter(image, display: bool = False):
+    """
+    This function applies the High Frequency Emphasis Filter on an Image.
+    if display is true, it will show the comparison before and after the filter operation.
+    """
+    # https://123machinelearn.wordpress.com/2017/12/25/image-enhancement-using-high-frequency-emphasis-filtering-and-histogram-equalization/
     image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     npFFT = np.fft.fft2(image_bw)
     npFFTS = np.fft.fftshift(npFFT)
@@ -150,7 +165,7 @@ def applyHFEFilter(image, display=False):
     # Perform IFFT (implemented here using the np.fft function)
     HFEfinal = (np.conjugate(fft2d(np.conjugate(HFE)))) / (P * Q)
 
-    output = np.sqrt((HFEfinal.real)**2 + (HFEfinal.imag)**2)
+    output = np.sqrt((HFEfinal.real) ** 2 + (HFEfinal.imag) ** 2)
     output = np.array(np.stack((output,) * 3, axis=-1), dtype=np.uint8)
 
     if display:
@@ -159,7 +174,9 @@ def applyHFEFilter(image, display=False):
 
         # Display the original image
         fig.add_subplot(rows, cols, 1)
-        plt.imshow(image, cmap=plt.cm.gray); plt.axis('off'); plt.title("Input Image")
+        plt.imshow(image, cmap=plt.cm.gray);
+        plt.axis('off');
+        plt.title("Input Image")
 
         # Display the thresholded image
         fig.add_subplot(rows, cols, 2)
