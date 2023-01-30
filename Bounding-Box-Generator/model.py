c17f5909e1905b72d73e5bc38568b92d4f5c90bc
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-01-30 14:23:09 -0500

    Added the model file

---
+++ Bounding-Box-Generator/model.py
@@ -0,0 +1,20 @@
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+
+
+def get_object_detection_model(num_classes):
+    # load a model pre-trained pre-trained on COCO
+    model = fasterrcnn_resnet50_fpn(pretrained=True)
+
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    return model
+
+
+
+if __name__ == "__main__":
+    model = get_object_detection_model(num_classes=2)
+    print(model)
