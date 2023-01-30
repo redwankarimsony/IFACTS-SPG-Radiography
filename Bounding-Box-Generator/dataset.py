f257706f2dbc0f73be732224bf130d54a03cb3d4
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-01-30 14:22:52 -0500

    added the dataset file

--- Bounding-Box-Generator/dataset.py
+++ Bounding-Box-Generator/dataset.py
@@ -3,17 +3,20 @@ import cv2
 import torch
 import numpy as np
 
+from matplotlib import patches
+
 from xml.etree import ElementTree as et
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, Subset
+from pytorch_vision_library import utils
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
 
 
-
-# defining the files directory and testing directory
-files_dir = '../input/fruit-images-for-object-detection/train_zip/train'
-test_dir = '../input/fruit-images-for-object-detection/test_zip/test'
-
-
-class FruitImagesDataset(torch.utils.data.Dataset):
+class ObjectDetectionDataset(Dataset):
+    """
+    make sure you have all the images in the same folder and the
+    label file is pascal voc xml format for each of the image.
+    """
 
     def __init__(self, files_dir, width, height, transforms=None):
         self.transforms = transforms
@@ -23,30 +26,28 @@ class FruitImagesDataset(torch.utils.data.Dataset):
 
         # sorting the images for consistency
         # To get images, the extension of the filename is checked to be jpg
-        self.imgs = [image for image in sorted(os.listdir(files_dir))
-                     if image[-4:] == '.jpg']
+        self.imgs = self.list_image_files(self.files_dir)
 
         # classes: 0 index is reserved for background
-        self.classes = [_, 'apple', 'banana', 'orange']
+        self.classes = ["background", 'bone']
 
     def __getitem__(self, idx):
 
-        img_name = self.imgs[idx]
-        image_path = os.path.join(self.files_dir, img_name)
+        image_path = self.imgs[idx]
+        # image_path = os.path.join(self.files_dir, img_name)
 
         # reading the images and converting them to correct size and color
         img = cv2.imread(image_path)
+        print(img.shape)
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)
         img_res = cv2.resize(img_rgb, (self.width, self.height), cv2.INTER_AREA)
         # diving by 255
         img_res /= 255.0
 
         # annotation file
-        annot_filename = img_name[:-4] + '.xml'
-        annot_file_path = os.path.join(self.files_dir, annot_filename)
+        annot_file_path = image_path[:-4] + '.xml'
 
-        boxes = []
-        labels = []
+        boxes, labels = [], []
         tree = et.parse(annot_file_path)
         root = tree.getroot()
 
@@ -105,11 +106,114 @@ class FruitImagesDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.imgs)
 
+    @staticmethod
+    def list_image_files(img_dir, exts=["JPG", "PNG", "BMP", "JPEG"]):
+        all_files = os.listdir(img_dir)
+        exts = exts + [x.lower() for x in exts]
+        img_files = []
+        for file_path in all_files:
+            ext = file_path.split(".")[-1]
+            if ext in exts:
+                img_files.append(os.path.join(img_dir, file_path))
+        return img_files
 
-# check dataset
-dataset = FruitImagesDataset(files_dir, 224, 224)
-print('length of dataset = ', len(dataset), '\n')
+    @staticmethod
+    def plot_img_bbox(img, target):
+        # plot the image and bboxes
+        # Bounding boxes are defined as follows: x-min y-min width height
+        fig, a = plt.subplots(1, 1)
+        fig.set_size_inches(5, 5)
+        a.imshow(img)
+        for box in (target['boxes']):
+            x, y, width, height = box[0], box[1], box[2] - box[0], box[3] - box[1]
+            rect = patches.Rectangle((x, y),
+                                     width, height,
+                                     linewidth=2,
+                                     edgecolor='r',
+                                     facecolor='none')
 
-# getting the image and target for a test index.  Feel free to change the index.
-img, target = dataset[78]
-print(img.shape, '\n', target)
+            # Draw the bounding box on top of the image
+            a.add_patch(rect)
+        plt.savefig("labeled_plot.png", dpi=300)
+
+
+# Send train=True fro training transforms and False for val/test transforms
+def get_transform(train):
+    if train:
+        return A.Compose([
+            A.HorizontalFlip(0.5),
+            # ToTensorV2 converts image to pytorch tensor without div by 255
+            A.RandomBrightnessContrast(p=0.2),
+            ToTensorV2(p=1.0)
+        ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+    else:
+        return A.Compose([
+            ToTensorV2(p=1.0)
+        ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+
+
+def load_dataset(config):
+    # use our dataset and defined transformations
+    dataset = ObjectDetectionDataset(config["train_dir"],
+                                     config["width"],
+                                     config["height"],
+                                     transforms=get_transform(train=True))
+    dataset_test = ObjectDetectionDataset(config["test_dir"],
+                                          config["width"],
+                                          config["height"],
+                                          transforms=get_transform(train=False))
+
+    # split the dataset in train and test set
+    torch.manual_seed(1)
+    indices = torch.randperm(len(dataset)).tolist()
+
+    # train test split
+    test_split = 0.2
+    tsize = int(len(dataset) * test_split)
+    dataset = Subset(dataset, indices[:-tsize])
+    dataset_valid = Subset(dataset_test, indices[-tsize:])
+
+    # define training and validation data loaders
+    dl_train = DataLoader(dataset,
+                          batch_size=config["batch_size"],
+                          shuffle=True,
+                          num_workers=config["num_workers"],
+                          collate_fn=utils.collate_fn)
+
+    dl_valid = DataLoader(dataset_valid,
+                          batch_size=config["batch_size"],
+                          shuffle=True,
+                          num_workers=config["num_workers"],
+                          collate_fn=utils.collate_fn)
+
+    dl_test = DataLoader(dataset_test,
+                         batch_size=config["batch_size"] * 2,
+                         shuffle=False,
+                         num_workers=config["num_workers"],
+                         collate_fn=utils.collate_fn)
+
+    return dl_train, dl_valid, dl_test
+
+
+if __name__ == "__main__":
+    # defining the files directory and testing directory
+    files_dir = './dummy_dataset'
+
+    # check dataset
+    dataset = ObjectDetectionDataset(files_dir, 224, 224)
+    print('length of dataset = ', len(dataset), '\n')
+
+    # # getting the image and target for a test index.  Feel free to change the index.
+    img, target = dataset[78]
+    print(img.shape, '\n', target)
+
+    import matplotlib.pyplot as plt
+
+    plt.imshow(img)
+    plt.savefig("output.png")
+
+    # Function to visualize bounding boxes in the image
+
+    # plotting the image with bboxes. Feel free to change the index
+    img, target = dataset[25]
+    # plot_img_bbox(img, target)
