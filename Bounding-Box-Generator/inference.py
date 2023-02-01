e82425af4f7022363aad0cb4bf6f7dce6e7907b5
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-01-31 21:35:18 -0500

    completed the inference script to save the labels in YOLO format

--- Bounding-Box-Generator/inference.py
+++ Bounding-Box-Generator/inference.py
@@ -1,13 +1,17 @@
 import torch
 import argparse
 import json
+import os
 import torchvision
-from torchvision import transforms
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
 from model import get_object_detection_model, load_checkpoint
+from torch.utils.data import DataLoader
 
-from dataset import load_dataset
+from dataset import load_dataset, coco_2_yolo
 
-from dataset import ObjectDetectionDataset, get_transform
+from dataset import ObjectDetectionInferenceDataset, get_transform, plot_img_bbox
 
 
 # the function takes the original prediction and the iou threshold.
@@ -33,22 +37,48 @@ def apply_nms(orig_prediction, iou_thresh=0.3):
     return final_prediction
 
 
-def torch_to_pil(img):
-    """
-    Args:
-        img: a torch.tensor image [C x W x H]
 
-    Returns: PIL image of the same size
-
-    """
-    return transforms.ToPILImage()(img).convert('RGB')
 
 
 def main(config):
-    dataset_test = ObjectDetectionDataset(config["test_dir"],
-                                          config["width"],
-                                          config["height"],
-                                          transforms=get_transform(train=False))
+    # Load the model
+    # to train on gpu if selected.
+    device = torch.device(config["cuda_device"]) if torch.cuda.is_available() else torch.device('cpu')
+    num_classes = 2
+    model = get_object_detection_model(num_classes=2)
+    # model = nn.DataParallel(model)
+    model = model.to(device)
+    checkpoint = torch.load(f"saved_models/best_model.pt")
+    model.load_state_dict(checkpoint['state_dict'])
+
+    ds_infer = ObjectDetectionInferenceDataset(config["infer_dir"],
+                                               config["width"],
+                                               config["height"],
+                                               transforms=A.Compose([ToTensorV2(p=1.0)]))
+
+    dl_infer = DataLoader(ds_infer,
+                          num_workers=config["num_workers"],
+                          shuffle=False,
+                          pin_memory=config["pin_memory"])
+
+    model.eval()
+
+    with torch.no_grad():
+        for epoch, (imgs, img_paths) in enumerate(dl_infer):
+            predictions = model(imgs.to(device))
+            for prediction, img_path in zip(predictions, img_paths):
+                prediction = apply_nms(prediction, iou_thresh=0.7)
+                if len(prediction["boxes"]):
+                    label_file_name = img_path.split("/")[-1].replace(".png", ".txt")
+                    with open(os.path.join(config["infer_save_dir"], label_file_name ), "w") as fp:
+                        [label, x, y, w, h] = coco_2_yolo(prediction)
+                        print(f"{label-1} {x:0.4f} {y:0.4f} {w:0.4f} {h:0.4f}", file=fp)
+                else:
+                    print(f"No prediction on {img_path}")
+
+
+
+
 
 
 if __name__ == "__main__":
