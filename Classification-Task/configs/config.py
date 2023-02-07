d7ca6ffe4ff84d06505c2e83b926121ae691f005
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-02-06 20:33:37 -0500

    added configuration file

---
+++ Classification-Task/configs/config.py
@@ -0,0 +1,14 @@
+# config/config.py
+# Created by sonymd at 2/5/23
+# Mail: sonymd@msu.edu
+# GitHub: www.github.com/redwankarimsony
+# Graduate Researcher, iPRoBe Lab, CSE, MSU
+
+
+config = {
+    "experiment_name": 'base_experiment',
+    "img_dir": "/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/images_annotation_2",
+    "annot_dir": "/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/Three_ROI_280_identities",
+    "width": 224,
+    "height": 224,
+    "valid_ratio": 30}
---
+++ Classification-Task/dataset.py
@@ -0,0 +1,12 @@
+# Created by sonymd at 2/5/23
+# Mail: sonymd@msu.edu
+# GitHub: www.github.com/redwankarimsony
+# Graduate Reseracher, iPRoBe Lab, CSE, MSU
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
