f4a4af31d13dfee82a2cd074d64fca539391eee0
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-02-07 12:41:48 -0500

    renamed the box_preset variable in config

--- Classification-Task/configs/config.py
+++ Classification-Task/configs/config.py
@@ -12,9 +12,13 @@ class configClass:
         self.results_dir = f"results/{self.experiment_name}"
         self.img_dir = "/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/images_annotation_2"
         self.annot_dir = "/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/Three_ROI_280_identities"
-        self.width = 224
-        self.height = 224
+        self.width = 512
+        self.height = 512
         self.valid_ratio = 0.5
+        self.box_preset = 0    #[0=(T1-T5 segment only),
+                        # 1= (Whole Vertebral Column Only)
+                        # 2= (T1-T5) with Clavicle
+                        # 3 =(Consider Whole Xray)]
 
 
 
