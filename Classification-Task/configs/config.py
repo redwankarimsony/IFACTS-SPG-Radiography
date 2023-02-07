761803783dcced8b1344607774a9110bcedf4b6b
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-02-06 21:51:54 -0500

    added the partial dataset and config file

--- Classification-Task/configs/config.py
+++ Classification-Task/configs/config.py
@@ -3,12 +3,29 @@
 # Mail: sonymd@msu.edu
 # GitHub: www.github.com/redwankarimsony
 # Graduate Researcher, iPRoBe Lab, CSE, MSU
+import os
 
-class cfg:
+
+class configClass:
     def __init__(self):
         self.experiment_name = 'base_experiment'
-        self.img_dir= "/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/images_annotation_2"
-        self.annot_dir= "/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/Three_ROI_280_identities"
-        self.width= 224
-        self.height= 224
-        self.valid_ratio=30
+        self.results_dir = f"results/{self.experiment_name}"
+        self.img_dir = "/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/images_annotation_2"
+        self.annot_dir = "/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/Three_ROI_280_identities"
+        self.width = 224
+        self.height = 224
+        self.valid_ratio = 0.5
+
+
+
+        self.make_results_dir()
+
+    def make_results_dir(self):
+        try:
+            os.makedirs(self.results_dir, exist_ok=False)
+        except Exception as e:
+            print(e)
+
+
+cfg = configClass()
+
--- Classification-Task/dataset.py
+++ Classification-Task/dataset.py
@@ -7,57 +7,119 @@ import os
 import torch
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
+from configs.config import cfg
+import math
 
 
 class XrayDataset(Dataset):
-    def __init__(self, data_dir, split="train", width=224, height=224, transform=None):
+    def __init__(self, cfg, split="train", transform=None):
         super(XrayDataset, self).__init__()
-        self.data_dir = data_dir
+        self.img_dir = cfg.img_dir
+        self.annot_dir = cfg.annot_dir
         self.split = split
-        self.width = width
-        self.height = height
+        self.width = cfg.width
+        self.height = cfg.height
         self.transform = transform
-        self.img_paths = None
+        self.df = pd.read_csv(os.path.join(cfg.results_dir, "data_splits.csv"))
+        self.df = self.df[self.df["split"] == self.split].reset_index(drop=True)
+        self.labels = {}
+
+        self._load_labels()
+
+        print(self.labels)
 
     def __len__(self):
-        return len(self.img_paths)
+        return len(self.df)
 
     def __getitem__(self, idx):
-        pass
+        img_path = os.path.join(self.img_dir, f"{self.df.iloc[idx, 0]}.png")
+        annot_path = os.path.join(self.img_dir, f"{self.df.iloc[idx, 0]}.txt")
+
+    def _load_labels(self):
+        temp = pd.read_csv(f"{cfg.results_dir}/labels_mapping.csv")
+        for idx, row in temp.iterrows():
+            self.labels[row["ID"]] = row["label_idx"]
 
 
-def split_dataset(
-        annotation_dir="/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/Three_ROI_280_identities"):
-    all_files, label_files, ID_labels = os.listdir(annotation_dir), [], {}
 
+def split_dataset(annot_dir=cfg.annot_dir):
+    all_files, label_files, ID_labels = os.listdir(annot_dir), [], {}
+    train_files, valid_files, IDs = [], [], set({})
+
+    # Listing all the label files
     for _file in all_files:
         if _file.endswith(".txt"):
             label_files.append(_file)
             ID = _file.split("_")[0]
+            IDs.add(ID)
             if ID in ID_labels.keys():
                 ID_labels[ID].append(_file)
             else:
                 ID_labels[ID] = [_file, ]
 
-    # Printing out the
-    with open("results/data_stat.csv", "w") as fp:
+    # Printing out the data statistics
+    with open(f"{cfg.results_dir}/data_stat.csv", "w") as fp:
         print("img_count,id_count", file=fp)
         for i in range(1, 15):
             count = 0
             for key in ID_labels.keys():
-                if len(ID_labels[key]) == i:
+                _files = ID_labels[key]
+                if len(_files) == i:
                     count += 1
-            print(f"{i},{count}", file=fp)
+                print(f"{i},{count}", file=fp)
 
+    # splitting up the train and validation images.
+    for ID in IDs:
+        _files = ID_labels[ID]
+        if len(_files) == 1:
+            train_files.extend(_files)
+        elif len(_files) == 2:
+            train_files.append(_files[0])
+            valid_files.append(_files[1])
+        elif len(_files) == 3:
+            train_files.extend(_files[:2])
+            valid_files.extend(_files[2:])
+        else:
+            num_t = int(math.ceil(len(_files) * (1 - cfg.valid_ratio)))
+            train_files.extend(_files[:num_t])
+            valid_files.extend(_files[num_t:])
+            # if len(_files[num_t:])==1:
+            #     valid_files.append(_files[num_t:][0])
+            # else:
+            #     valid_files.extend(_files[num_t:])
 
+    print(f"Total Images: {len(all_files)}",
+          f"\nTrain Images: {len(train_files)}",
+          f"\nValid Images: {len(valid_files)}")
 
+    # Making the data frame for later reference
+    image_ids, ids, splits = [], [], []
+    for _file in train_files:
+        image_ids.append(_file.replace(".txt", ""))
+        ids.append(_file.split("_")[0]),
+        splits.append("train")
 
+    for _file in valid_files:
+        image_ids.append(_file.replace(".txt", ""))
+        ids.append(_file.split("_")[0]),
+        splits.append("valid")
 
+    df = pd.DataFrame({"image_id": image_ids,
+                       "id": ids,
+                       "split": splits})
+    csv_path = os.path.join(cfg.results_dir, "data_splits.csv")
+    df.to_csv(csv_path, index=False)
+    print(f"Splits written at : ./{csv_path}")
+
+    # Writing out the labels mapping to index
+    IDs = sorted(list(IDs))
+    with open(f"{cfg.results_dir}/labels_mapping.csv", "w") as fp:
+        print("ID,""label_idx", file=fp)
+        for idx, ID in enumerate(IDs):
+            print(f"{ID}, {idx}", file=fp)
 
 
 if __name__ == "__main__":
-    ds = XrayDataset(
-        data_dir="/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/Annotations_T1-T5_280_identities",
-        split="train")
-
     split_dataset()
+    ds = XrayDataset(cfg, split="valid", transform=None)
+    print(len(ds))
