997bff6b62b79a958eb764061cf5772d9455ef9c
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-02-08 00:49:43 -0500

    changed the minor thigns in dataloader

--- Classification-Task/dataset.py
+++ Classification-Task/dataset.py
@@ -12,7 +12,7 @@ import numpy as np
 import torch
 import pandas as pd
 import matplotlib.pyplot as plt
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as tf
 from configs.config import cfg
 
@@ -45,7 +45,7 @@ class XrayDataset(Dataset):
         annot_path = osp.join(self.annot_dir, f"{self.df.iloc[idx, 0]}.txt")
 
         df = pd.read_csv(annot_path, delimiter=" ", names=["label", "x", "y", "w", "h"], header=None)
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread(img_path)[:, :, 0]  # , cv2.IMREAD_GRAYSCALE)
         img_mask = np.zeros_like(img)
         H, W = img.shape
 
@@ -59,15 +59,20 @@ class XrayDataset(Dataset):
                                                   take_square=False)
         img_mask[ymin:ymax, xmin:xmax] = img[ymin:ymax, xmin:xmax]
 
-        print(xmin, ymin, xmax, ymax, img.shape, self.df.iloc[idx, 0])
+        # print(xmin, ymin, xmax, ymax, img.shape, self.df.iloc[idx, 0])
 
+        img_stacked = np.array([img, img_mask, img])
+        xmin, ymin, xmax, ymax = self.bbox2points([x, y, w, h], H, W,
+                                                  box_preset=self.box_preset,
+                                                  take_square=True)
+        img_final_masked = img_stacked[:, ymin:ymax, xmin:xmax]
 
-
-        # img = torch.tensor(img / 255.0)
-        # if self.transform:
-        #     img = self.transform(img)
-        # print(img)
-        return [img, img_mask], " ", self.df.iloc[idx, 0]
+        img_tensor = torch.tensor(img_final_masked) / 255.
+        # print(img_tensor.shape)
+        if self.transform:
+            img_tensor = self.transform(img_tensor)
+        # return [img, img_mask, img_stacked, img_tensor], " ", self.df.iloc[idx, 0]
+        return img_tensor, self.labels[self.df.iloc[idx, 1]], self.df.iloc[idx, 0]
 
     def _load_labels(self):
         temp = pd.read_csv(f"{cfg.results_dir}/labels_mapping.csv")
@@ -105,17 +110,53 @@ class XrayDataset(Dataset):
             ymax = int((y + (h / 2)) * H)
         return xmin, ymin, xmax, ymax
 
+    def display_example(self, idx):
+        [img, img_mask, img_stacked, img_final_masked], label, file_id = self[idx]
+        print(img.shape)
+
+        print(img)
+        print((img + img_mask) // 2)
+
+        fig = plt.figure(figsize=(15, 15), dpi=100)
+        fig.add_subplot(2, 3, 1)
+
+        plt.imshow(img, cmap=plt.cm.gray)
+        plt.title("Original Image")
+
+        fig.add_subplot(2, 3, 2)
+        plt.imshow(img_mask, cmap=plt.cm.gray)
+        plt.title("Masked Image")
+
+        fig.add_subplot(2, 3, 3)
+        plt.imshow((img + img_mask) // 2, cmap=plt.cm.gray)
+        plt.title("Average Channel")
+
+        fig.add_subplot(2, 3, 4)
+        plt.imshow(np.maximum(img, img_mask), cmap=plt.cm.gray)
+        plt.title("Reconstruction")
+
+        fig.add_subplot(2, 3, 5)
+        plt.imshow(np.moveaxis(img_stacked, 0, 2), cmap=plt.cm.gray)
+        plt.title("Stacked")
+
+        fig.add_subplot(2, 3, 6)
+        plt.imshow(np.moveaxis(img_final_masked.numpy(), 0, 2), cmap=plt.cm.gray)
+        plt.title("Stacked and Cropped")
+
+        fig.suptitle(f"Comparison of Different Channels\n{file_id}")
+        plt.show()
+
 
 def split_dataset(annot_dir=cfg.annot_dir):
     all_files, label_files, ID_labels = os.listdir(annot_dir), [], {}
-    train_files, valid_files, IDs = [], [], set({})
+    train_files, valid_files, _IDs = [], [], set({})
 
     # Listing all the label files
     for _file in all_files:
         if _file.endswith(".txt"):
             label_files.append(_file)
             ID = _file.split("_")[0]
-            IDs.add(ID)
+            _IDs.add(ID)
             if ID in ID_labels.keys():
                 ID_labels[ID].append(_file)
             else:
@@ -133,7 +174,7 @@ def split_dataset(annot_dir=cfg.annot_dir):
                 print(f"{i},{count}", file=fp)
 
     # splitting up the train and validation images.
-    for ID in IDs:
+    for ID in _IDs:
         _files = ID_labels[ID]
         if len(_files) == 1:
             train_files.extend(_files)
@@ -161,7 +202,7 @@ def split_dataset(annot_dir=cfg.annot_dir):
 
     for _file in valid_files:
         image_ids.append(_file.replace(".txt", ""))
-        ids.append(_file.split("_")[0]),
+        ids.append(_file.split("_")[0])
         splits.append("valid")
 
     df = pd.DataFrame({"image_id": image_ids, "id": ids, "split": splits})
@@ -170,46 +211,33 @@ def split_dataset(annot_dir=cfg.annot_dir):
     print(f"Splits written at : ./{csv_path}")
 
     # Writing out the labels mapping to index
-    IDs = sorted(list(IDs))
+    _IDs = sorted(list(_IDs))
     with open(f"{cfg.results_dir}/labels_mapping.csv", "w") as fp:
         print("ID,""label_idx", file=fp)
-        for idx, ID in enumerate(IDs):
+        for idx, ID in enumerate(_IDs):
             print(f"{ID}, {idx}", file=fp)
 
 
-
-
-
-
-
 transfroms = torch.nn.Sequential(
-    tf.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)))
+    tf.Resize(512),
+    tf.CenterCrop(512),
+    tf.Normalize(mean=cfg.stat_mean, std=cfg.stat_std))
+
 if __name__ == "__main__":
     split_dataset()
-    ds = XrayDataset(cfg, split="valid", transform=None)
+    ds = XrayDataset(cfg, split="valid", transform=transfroms)
     print(len(ds))
-    [img, img_mask], label, file_id = ds[0]
-    print(img.shape)
+    dl = DataLoader(ds, batch_size=cfg.batch_size, shuffle=True, num_workers=cfg.num_workers, pin_memory=cfg.pin_memory)
+    figure = plt.figure(figsize=(20, 20), dpi=100)
+    X, Y, IDs = next(iter(dl))
 
-    print(img)
-    print((img + img_mask) // 2)
+    for i, [x, y, id] in enumerate(zip(X, Y, IDs)):
+        # x, y, id = ds[i]
+        x = np.moveaxis(x.numpy(), 0, 2)
 
-    fig = plt.figure(figsize=(15, 15), dpi=200)
-    fig.add_subplot(2, 2, 1)
-
-    plt.imshow(img, cmap=plt.cm.gray)
-    plt.title("Original Image")
-
-    fig.add_subplot(2, 2, 2)
-    plt.imshow(img_mask, cmap=plt.cm.gray)
-    plt.title("Masked Image")
-
-    fig.add_subplot(2,2,3)
-    plt.imshow((img+img_mask)//2, cmap=plt.cm.gray)
-    plt.title("Average Channel")
-
-    fig.add_subplot(2,2,4)
-    plt.imshow(np.maximum(img, img_mask), cmap=plt.cm.gray)
-    plt.title("Reconstruction")
-    fig.suptitle(f"Comparison of Different Channels\n{file_id}")
+        img = x * np.array(cfg.stat_std) + np.array(cfg.stat_mean)
+        figure.add_subplot(4, 4, i + 1)
+        plt.imshow(np.clip(img,a_min=0, a_max=1 ), cmap=plt.cm.gray)
+        plt.title(id)
+        plt.axis('off')
     plt.show()
