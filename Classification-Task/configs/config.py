056267d0c7ebf4aafd49514c907c613020be4c19
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-02-11 08:33:00 -0500

    added the assertion check in the input image size

--- Classification-Task/configs/config.py
+++ Classification-Task/configs/config.py
@@ -16,13 +16,19 @@ class configClass:
         self.annot_dir = "/home/sonymd/Desktop/IFACTS/IFACTS-SPG-Radiography/Annotation/Three_ROI_280_identities"
         self.width = 512
         self.height = 512
+        assert self.width == self.height "The height and width of the input image should be the same"
+        self.limit_train_batches = 28
+        self.max_epoch = 500
+
         self.valid_ratio = 0.5
-        self.box_preset = 0  # [0=(T1-T5 segment only),
+
+        # [0=(T1-T5 segment only),
         # 1= (Whole Vertebral Column Only)
         # 2= (T1-T5) with Clavicle
         # 3 =(Consider Whole Xray)]
+        self.box_preset = 1
 
-        self.model_arch = "densenet121"
+        self.model_arch = "efficientnet_b3"
         self.num_classes = 281
         self.pin_memory = True
         self.cuda_precision = "highest"  # ["medium", "high", "highest"]
@@ -32,16 +38,17 @@ class configClass:
 
         self.batch_size = 16
         self.num_workers = min(self.batch_size, int(os.cpu_count() * 0.8))
-        self.learning_rate = 0.0001
+        self.learning_rate = 1e-4
 
-        self.tfms_train = Sequential(tf.Resize(512),
-                                     tf.CenterCrop(512),
-                                     tf.RandomRotation(degrees=5),
+        self.tfms_train = Sequential(tf.Resize(self.width),
+                                     tf.CenterCrop(self.width),
+                                     tf.RandomRotation(degrees=8),
+                                     tf.RandomPerspective(distortion_scale=0.2, p=0.3),
                                      tf.RandomAdjustSharpness(sharpness_factor=1.3, p=0.6),
                                      tf.Normalize(mean=self.stat_mean, std=self.stat_std))
 
-        self.tfms_valid = Sequential(tf.Resize(512),
-                                     tf.CenterCrop(512),
+        self.tfms_valid = Sequential(tf.Resize(self.width),
+                                     tf.CenterCrop(self.width),
                                      # tf.RandomRotation(degrees=5),
                                      # tf.RandomAdjustSharpness(sharpness_factor=1.3, p=0.6),
                                      tf.Normalize(mean=self.stat_mean, std=self.stat_std))
