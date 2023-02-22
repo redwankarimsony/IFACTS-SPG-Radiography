0434706a2128dac763d3a000d03aeb7e7cab67cf
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-02-22 01:34:22 -0500

    added the inference script

--- Classification-Task/inference.py
+++ Classification-Task/inference.py
@@ -7,7 +7,10 @@
 
 import torch
 import pytorch_lightning as pl
+from torch.utils.data import DataLoader
 from dataset import XrayDataset
+from train import LightningTrainer
+from sklearn.metrics import confusion_matrix
 
 
 
@@ -17,7 +20,37 @@ from configs.config import cfg
 
 def inference(cfg):
     ds_valid = XrayDataset(cfg, split="valid", transform=cfg.tfms_valid)
+    dl_valid = DataLoader(ds_valid,
+                          batch_size=cfg.batch_size,
+                          shuffle=False,
+                          num_workers=cfg.num_workers,
+                          pin_memory=cfg.pin_memory,
+                          persistent_workers=True)
     print(len(ds_valid))
+    model = LightningTrainer.load_from_checkpoint("saved_checkpoints/box_2/densenet121/densenet121_epoch=308-val_acc_epoch=0.7848.ckpt")
+
+    predictions = []
+    gt = []
+    ml_model = model.model.to("cuda:7")
+    with torch.no_grad():
+        for idx, batch in enumerate(dl_valid):
+            X, y, codes = batch
+            print(y.shape)
+            y_hat = ml_model(X.to("cuda:7"))
+            preds = model.softmax(y_hat)
+            predictions.append(preds.cpu())
+            gt.extend(y.tolist())
+        all_results = torch.vstack(predictions)
+        final_predict = torch.argmax(all_results, axis=1).tolist()
+        print(len(final_predict), len(gt))
+
+
+        print(final_predict[:5])
+        print(gt[:5])
+
+
+        conf_matrix = confusion_matrix(gt, final_predict)
+        print(conf_matrix[:45, :45])
 
 
 
