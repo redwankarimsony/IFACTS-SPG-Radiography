36c44e39b7ee1a48f7eb954ac3ca99c7533f2c8f
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-10-13 11:44:32 -0400

    The Old Easy hard configuration

---
+++ configs/config_easy_hard.py
@@ -0,0 +1,73 @@
+
+# config/config.py
+# Created by sonymd at 2/5/23
+# Mail: sonymd@msu.edu
+# GitHub: www.github.com/redwankarimsony
+# Graduate Researcher, iPRoBe Lab, CSE, MSU
+
+
+import os
+import torchvision.transforms as tf
+from torch.nn import Sequential
+
+
+class configClass:
+    def __init__(self):
+        self.experiment_name = '680_IDs_easy_hard'
+        self.results_dir = f"results/{self.experiment_name}"
+        self.checkpoints_dir = "easy_hard"
+        self.img_dir = "../New Annotation/Matches_e_d_three_ROI_images"
+        self.annot_dir = "../New Annotation/Matches_e_d_three_ROI_annotations"
+        self.width = 512
+        self.height = 512
+        assert self.width == self.height, "The height and width of the input image should be the same"
+        self.limit_train_batches = 28
+        self.max_epoch = 1200
+
+        self.valid_ratio = 0.5
+
+        #   0=(T1-T5 segment only),
+        #   1= (Whole Vertebral Column Only)
+        #   2= (T1-T5) with Clavicle
+        #   3 =(Consider Whole Xray)
+        self.box_preset = 0
+
+        self.model_arch = "resnet34"
+        self.cuda_devices = [3]
+
+        self.num_classes = 680
+        self.pin_memory = True
+        self.use_cache=True
+        self.cuda_precision = "high"  # ["medium", "high", "highest"]
+
+        self.stat_mean = [0.485, 0.456, 0.406]
+        self.stat_std = [0.229, 0.224, 0.225]
+        
+        self.batch_size = 16
+        self.num_workers = min(self.batch_size, int(os.cpu_count() * 0.8))
+        self.learning_rate = 1e-4
+
+        self.tfms_train = Sequential(tf.Resize(self.width),
+                                     tf.CenterCrop(self.width),
+                                     tf.RandomRotation(degrees=8),
+                                     # tf.RandomEqualize(p=0.3), #new
+                                     tf.RandomPerspective(distortion_scale=0.2, p=0.3),
+                                     tf.RandomAdjustSharpness(sharpness_factor=1.3, p=0.3),
+                                     # tf.ColorJitter(brightness=.5, hue=.3), # new
+                                     tf.Normalize(mean=self.stat_mean, std=self.stat_std))
+
+        self.tfms_valid = Sequential(tf.Resize(self.width),
+                                     tf.CenterCrop(self.width),
+                                     tf.Normalize(mean=self.stat_mean, std=self.stat_std))
+
+    def make_results_dir(self):
+        try:
+            os.makedirs(self.results_dir, exist_ok=True)
+            os.makedirs(self.checkpoints_dir, exist_ok=True)
+        except Exception as e:
+            print(e)
+
+
+
+cfg = configClass()
+cfg.make_results_dir()
