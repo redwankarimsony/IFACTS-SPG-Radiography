c8eecf984aae4ce8f4e6dd70e8904099820aaf14
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2023-10-19 14:41:34 -0400

    Summary is generated for a single experiment

--- summarize.py
+++ summarize.py
@@ -8,114 +8,139 @@
 
 import os
 import pandas as pd
-import pickle
-from accuracy_top_k import select_the_best_model, inference
-from configs.config import configClass
+import argparse
 
 
 
-def generate_train_summary(results_dir="./temporal_sort", 
-                           models = ["resnet34",],
-                           box_presets = [0, 1, 2, 3]):
-    df = pd.DataFrame()
-    df["box_preset"] = box_presets
 
-    def find_best_model(model_dir):
-        files = os.listdir(model_dir)
-        ckpts = [float("0."+x.split("=")[-1].split(".")[1]) for x in files if x.endswith(".ckpt")]
-        return max(ckpts)
+def get_the_best_model(checkpoint_dir, ext=".ckpt"):
+    """
+    Returns the file path of the best model in the given checkpoint directory
 
-    for model_arch in models:
-        res = []
-        for box in df["box_preset"]:
-            model_dir = os.path.join(results_dir, f"box_{box}", model_arch)
-            if os.path.exists(model_dir):
-                # print(box, model_arch, find_best_model(model_dir))
+    Args:
+        checkpoint_dir (_type_): Location of the checkpoint directory whree the models are saved
+        ext (str, optional): Extension of the checkpoints file. Defaults to ".ckpt".
 
-                res.append(find_best_model(model_dir=model_dir))
-            else:
-                res.append(0.0)
+    Returns:
+        _type_: Returns the checkpoint file with the best file accuracy
+    """
 
-        df[model_arch] = res
+    # List all the files in the checkpoint directory with given extension
+    files = os.listdir(checkpoint_dir)
+    # Get the checkpoint files
+    ckpts = [x for x in files if x.endswith(ext)]
+    # Return the best model
+    return  max(ckpts, key= lambda x: float(x.split("=")[-1].split(".")[0]))
 
 
-    df.to_csv(os.path.join(results_dir, "top-1-summary.csv"))
 
+
+def generate_train_summary(experiment_name:str, box_presets:list, model_names:list):
+    """
+    Generates a summary of the training results for the given experiment name, box presets and model names
+    """
+
+    all_models = []
+
+    for box_preset in box_presets:
+        for model_name in model_names:
+            checkpoint_dir = os.path.join(experiment_name, box_preset, model_name)
+            if os.path.exists(checkpoint_dir):
+                best_model = get_the_best_model(checkpoint_dir=checkpoint_dir)
+                all_models.append(best_model)
+    return all_models
+
+
+    
+
+def summarize_single_experiment(experiment_name:str, verbose=True):
+    """Given the location of the experiment directory, this function summarizes the results of the experiment
+
+    Args:
+        experiment_name (str): Location of the experimenets
+
+    Returns:
+        _type_: the summary of the best model for each box_preset and model_name
+    """
+
+
+    all_models = []
+
+    # List all the box presets
+    box_presets = [x for x in os.listdir(experiment_name) if os.path.isdir(os.path.join(experiment_name, x))]   
+
+    for box_preset in box_presets:
+        # List all the models for the given box preset
+        model_names = os.listdir(os.path.join(experiment_name, box_preset))
+
+        for model_name in model_names:
+            checkpoint_dir = os.path.join(experiment_name, box_preset, model_name)
+            try:
+                best_model = get_the_best_model(checkpoint_dir=checkpoint_dir)
+                all_models.append(best_model)
+            except Exception as e:
+                print(f"Error: {e}")
+                print(f"Could not find the best model for {checkpoint_dir}")
+
+    data = []
+
+    for idx, filepath in enumerate(all_models):
+        parts = filepath.split("_")
+
+        # If the model name starts with efficientnet, then the model name is
+        if parts[0].startswith("efficientnet"):
+            model_name = parts[0] + "_" + parts[1]
+            box_preset = parts[2]
+        
+        # Otherwise, the model name is the first part
+        else:
+            model_name = parts[0]
+            box_preset = parts[1]
+
+        # Find the validation accuracy:
+        val_acc  = float(parts[-1].split("=")[-1].replace(".ckpt", ""))
+        
+        # Print the results
+        if verbose:
+            if idx == 0:
+                    print(f"{'MODEL':<20} {'CATEGORY':<25} {'SCORE'}")
+                    print(f"{'-----':<20} {'--------':<25} {'-----'}")
+        
+            print(f"{model_name:<20} {box_preset:<25} {val_acc:.4f}")
+
+        # Append the results to the data list
+        data.append({"model_name": model_name, "box_preset": box_preset, "val_acc": val_acc, "saved_model": filepath})
+
+    # Create a dataframe from the results
+    df = pd.DataFrame(data)
+
+    # Sort the dataframe by validation accuracy
+    df = df.sort_values(by="val_acc", ascending=False)
+
+    # Save the dataframe to the experiment directory
+    df.to_csv(os.path.join(experiment_name, "summary.csv"), index=False, sep=",")
+
+    # Print the dataframe path
+    print(f"\n\nSummary saved to {os.path.join(experiment_name, 'summary.csv')}")
+    
     return df
-
-
-def generate_top_k_acc_summary(results_dir="./saved_iii", 
-                           models = ["resnet34",],
-                           box_presets = [0, 1, 2, 3],
-                           acc_order = 2):
     
-    df = pd.DataFrame()
-    df["box_preset"] = box_presets
-    results = {}
-    all_pred_scores= {}
-    for model_arch in models:
-        for box in box_presets:
-            model_dir = os.path.join(results_dir, f"box_{box}", model_arch)
-            if os.path.exists(model_dir):
-                best_model_path = select_the_best_model(save_dir=results_dir,
-                                                        model_arch=model_arch,
-                                                        box_preset=box)
-                
-                cfg = configClass()
-                cfg.model_arch = model_arch
-                cfg.cuda_devices = [7, ]
-                cfg.box_preset = box
 
-                acc_ks, gt, all_scores = inference(cfg, 
-                                       best_model_checkpoint=best_model_path,
-                                       acc_order=acc_order)
-                results[(model_arch, box)] = acc_ks
-                all_pred_scores[(model_arch, box)] = all_scores
-                if "gt" not in all_pred_scores.keys():
-                    print(type(gt))
-                    all_pred_scores[("gt", 0)] = gt
-            else:
-                print(model_arch, box , "is not there yet")
-        # df[model_arch] = res
 
-        # df.to_csv(os.path.join(results_dir, f"top-{acc_order}-summary.csv"), index=False)
-    with open(os.path.join(results_dir, f"all_acc_upto_rank_10.pkl"), "wb") as fp:
-        pickle.dump(results, fp)
 
-    with open(os.path.join(results_dir, f"all_model_scores.pkl"), "wb") as fp:
-        pickle.dump(all_pred_scores, fp)
 
-    return df
-
-    
-    
-    
 if __name__ == "__main__":
 
-    results_dir = "temporal_sort"
-    models = ["resnet34", "resnet50", "resnet101", "densenet121", "densenet161", "densenet169", "densenet201", 
-              "efficientnet_b0", "efficientnet_b1", "efficientnet_b2", "efficientnet_b3"]
+    # Parse the arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--experiment_name", type=str, default="experiments/base_experiment")
+    args = parser.parse_args()
 
-    summary = generate_train_summary(results_dir=results_dir,
-                           models=models,
-                           box_presets=[0,1,2,3])
-    
-    print(summary)
-
-    sum_k = generate_top_k_acc_summary(results_dir=results_dir,
-                                       models=models,
-                                       box_presets=[0,1,2,3],
-                                       acc_order=7)
-
+    # models = ["resnet34", "resnet50", "resnet101", "densenet121", "densenet161", "densenet169", "densenet201", 
+    #           "efficientnet_b0", "efficientnet_b1", "efficientnet_b2", "efficientnet_b3"]
+    # box_presets = ["t1-t5", "clavicle-only", "complete-vertebrae"]
 
     
-
-
-
-
-
-
-
-
-
-        
+    # Summarize the results
+    summarize_single_experiment(experiment_name=f"{args.experiment_name}", verbose=True)
+    
